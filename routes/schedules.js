b98e765f2a5478714cde9b4645bc7ea10f02542b
beccahoo <beccahoo@gmail.com>
2021-09-06 15:23:48 +1000

    filler code

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
-// Your code is here
-// router.get('/', (req, res) => {
-//     res.send();
-// })
+
+router.get('/', (req, res) => {
+    res.render('pages/schedules');
+})
 
 // router.get('/:id', (req, res) => {
 //     res.render();
 // })
 
-//BEC is going to edit here 
 
 module.exports = router;
