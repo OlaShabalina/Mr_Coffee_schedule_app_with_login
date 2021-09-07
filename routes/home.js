90143fb4fb391124d2e8b04ba3d35af3901a0138
OlaShabalina <o.shabalina@outlook.com>
2021-09-07 10:21:57 +1000

    Created ejs pages for the login and register forms

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require('express');
 const router = express.Router();
 const db = require('../database');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); // Password encryption
 
 router.get('/', (req, res) => {
     res.render('pages/home');
 })
+
+
 
 
 
