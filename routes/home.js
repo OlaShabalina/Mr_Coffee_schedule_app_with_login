5d198585f0a61f3468c630d549b45d6ae9423b86
Rebecca Hoo <68106959+beccahoo@users.noreply.github.com>
2021-09-07 19:16:41 +1000

    Revert "Schedules page complete showing schedules from database"

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const db = require('../database');
 const bcrypt = require('bcrypt');
 
-
 router.get('/', (req, res) => {
     res.render('pages/home');
 })
--- routes/schedules.js
+++ routes/schedules.js
@@ -2,42 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
-//read all data from databases && set data received as parameter for front end
-router.get("/", (req, res) => {
-    db.any('SELECT * FROM schedules')
-        .then(function(databaseschedules) {
-            console.log(databaseschedules)
-            res.render('pages/schedules', {
-                schedules:databaseschedules
-              });
-        })
-        
-        .catch(function(error) {
-            console.log(error)
-        });
-    });
-    
-//     router.get('/', (req, res) => {
-//         res.render('pages/home');
+// Your code is here
+// router.get('/', (req, res) => {
+//     res.send();
 // })
-
-router.post('/', (req, res) => {
-    console.log(req.body)
-    db.none('INSERT INTO schedules(username, day, start_at, end_at) VALUES($1, $2, $3, $4);', [req.body.username, req.body.day, req.body.start_at, req.body.end_at])
-    .then(() => {
-      res.redirect('/')
-    })
-    .catch(error => {
-      console.log(error)
-      res.send(error)
-    })
-  })
-  
-
-module.exports = router;
-
-
 
 // router.get('/:id', (req, res) => {
 //     res.render();
 // })
+
+
+module.exports = router;
