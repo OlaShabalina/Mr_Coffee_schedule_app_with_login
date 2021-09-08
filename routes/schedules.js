40c26381df92fb33339564afbd4013b9b310c3e8
OlaShabalina <o.shabalina@outlook.com>
2021-09-08 19:07:26 +1000

    merges pages from both branches

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,8 +2,50 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database');
 
-router.get('/', (req, res) => {
-    res.send('check')
-})
+//read all data from databases && set data received as parameter for front end
+router.get("/", (req, res) => {
+    db.any('SELECT * FROM schedules')
+        .then(function(databaseschedules) {
+            console.log(databaseschedules)
+            res.render('pages/schedules', {
+                schedules:databaseschedules
+              });
+        })
+        
+        .catch(function(error) {
+            console.log(error)
+        });
+    });
+    
+//     router.get('/', (req, res) => {
+//         res.render('pages/home');
+// })
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    db.none('INSERT INTO schedules(username, day, start_at, end_at) VALUES($1, $2, $3, $4);', [req.body.username, req.body.day, req.body.start_at, req.body.end_at])
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(error => {
+      console.log(error)
+      res.send(error)
+    })
+  })
+
+  router.get('/newschedule', (req, res) => {
+    res.render('pages/newschedule')
+  });
+
+  router.get('/:id', (req, res) => {
+    res.render('')
+  })
+  
 
 module.exports = router;
+
+//Adding new comment here as placeholder
+
+// router.get('/:id', (req, res) => {
+//     res.render();
+// })
