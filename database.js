9ed858241107758cba010af1348aa9805ab7f0af
OlaShabalina <o.shabalina@outlook.com>
2021-10-18 17:32:25 +1100

    changed the connection string to include production mode

--- database.js
+++ database.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 
 const pgp = require('pg-promise')();
 
-const connection = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/schedules_project4`;
+// db connection details separate for production and dev mode
+const connection = {
+  connectionString: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
+  ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : false
+}
 
 const db = pgp(connection);
 
